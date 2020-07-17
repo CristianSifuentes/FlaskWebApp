6bbe24eeb3a51033ba11fb1e427aa5fae0de4033
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-07-16 23:00:28 -0500

    20200717: We have implemented Threads for send async emails using threading

--- app/email.py
+++ app/email.py
@@ -1,11 +1,16 @@
+from threading import Thread
 from flask_mail import Message
 from flask import current_app, render_template
-from . import mail
+from . import mail, app
 
+def send_async_mail(message):
+    with app.app_context():
+      mail.send(message)
+    
 def welcome_mail(user):
     message = Message('Welcome', 
                       sender=current_app.config['MAIL_USERNAME'],
                       recipients=[user.email])
     message.html = render_template('email/welcome.html', user=user)
-    
-    mail.send(message)
+    thread = Thread(target=send_async_mail, args=[message])
+    thread.start()
